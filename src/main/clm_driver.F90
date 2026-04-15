7c57a1859b87e96dfb74fc8914f7b505788757cc
mvdebolskiy <nomatdebol@gmail.com>
2026-04-15 19:42:00 +0200

    fix indents

--- src/main/clm_driver.F90
+++ src/main/clm_driver.F90
@@ -1116,7 +1116,7 @@ contains
 
        ! Dry Deposition of chemical tracers (Wesely (1998) parameterizaion)
        !  TODO PUT FAteS DRYDEP wrapper here
-       if(use_fates.and. n_drydep >0)then
+       if (use_fates .and. n_drydep >0)then
           call clm_fates%wrap_drydep(nc,water_inst%waterdiagnosticbulk_inst, drydepvel_inst)
        end if 
        call t_startf('depvel')
